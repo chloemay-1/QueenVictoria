d663c732ffe77b5142fa51704167920ad6128b2c
chloemay-1 <chloewillis605@gmail.com>
2022-11-26 11:53:23 +0000

    added to CSS and JS

---
+++ assets/JS/script.js
@@ -0,0 +1,73 @@
+// Event listeners added for when royal family clicked //
+const qEliz = document.getElementById("qEliz");
+qEliz.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Granddaughter</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const prPhilip = document.getElementById("prPhilip");
+prPhilip.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Grandson</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const kChar = document.getElementById("kChar");
+kChar.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Great Grandson</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const prWil = document.getElementById("prWil");
+prWil.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Great Great Grandson</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const prHar = document.getElementById("prHar");
+prHar.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Great Great Grandson</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const prGeor = document.getElementById("prGeor");
+prGeor.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Great Great Great Grandson</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const priChar = document.getElementById("priChar");
+priChar.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Great Great Great Granddaughter</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const prLo = document.getElementById("prLo");
+prLo.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Great Great Great Grandson</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const prArh = document.getElementById("prArh");
+prArh.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Great Great Great Grandson</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const priLil = document.getElementById("priLil");
+priLil.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Great Great Great Granddaughter</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const priAnn = document.getElementById("priAnn");
+priAnn.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Great Granddaughter</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const zara = document.getElementById("zara");
+zara.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Great Great Granddaughter</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const peter = document.getElementById("peter");
+peter.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Great Great Grandson</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const prEd= document.getElementById("prEd");
+prEd.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Great Grandson</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const ladLo = document.getElementById("ladLo");
+ladLo.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Great Great Granddaughter</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const visJam = document.getElementById("visJam");
+visJam.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Great Great Grandson</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const priBe = document.getElementById("priBe");
+priBe.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Great Great Granddaughter</h1>" + "<h3>Please refresh your screen</h3>"
+})
+const priEug = document.getElementById("priEug");
+priEug.addEventListener("click", function() {
+  document.body.innerHTML = "<h1>Great Great Great Great Granddaughter</h1>" + "<h3>Please refresh your screen</h3>"
+})
